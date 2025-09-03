724cbb906f41e88bf5e98291df12f267a631f74d
A02385984 <a02385984@el120-10.ece.usu.edu>
2025-09-03 17:03:38 -0600

    First commit

---
+++ myandgate/myandgate.srcs/sim_1/new/testbench.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ns
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Utah State University
+// Engineer: ECE2700 Dr. Zhen Zhang
+// 
+// Create Date: 09/03/2025 03:45:12 PM
+// Design Name: Lab1
+// Module Name: testbench
+// Project Name: testbench
+// Target Devices: Basys3
+// Tool Versions: Vivado 2024.2.1
+// Description: 
+//  a trivial module to implement an and gate
+// Dependencies: 
+//  none
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//  reeeeeeeeeeeeeeee
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+module testbench(     );
+    reg in1, in2, clk;
+    wire out;
+    
+    myandgate DUT(.A(in1), .B(in2), .F(out) );
+    
+    initial begin
+        clk = 0;
+        in1 = 0;
+        in2 = 0;
+        forever #10 clk = ~clk;        
+     end
+     
+     always @(posedge clk)begin
+        in1 = ~in1;
+        in2 = in2 + in1;
+     end
+    
+endmodule
---
+++ myandgate/myandgate.srcs/sources_1/new/myandgate.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ns
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Utah State University
+// Engineer: ECE2700 Dr. Zhen Zhang
+// 
+// Create Date: 09/03/2025 03:45:12 PM
+// Design Name: Lab1
+// Module Name: myandgate
+// Project Name: myandgate
+// Target Devices: Basys3
+// Tool Versions: Vivado 2024.2.1
+// Description: 
+//  a trivial module to implement an and gate
+// Dependencies: 
+//  none
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//  reeeeeeeeeeeeeeee
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module myandgate(
+//    output F,
+    output reg F,
+    input A,
+    input B
+    );
+    
+    // and U1(F,A,B); // original implementation
+
+//    assign F = A & B; // alternate 1
+
+    always @(A, B) begin
+	    F = A & B;
+    end
+
+endmodule
